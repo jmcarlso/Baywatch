d7970c697eb5ad4240ab871b3636402317759312
Joseph Carlson <guitarjoe96@yahoo.com>
2017-06-28 20:52:50 -0400

    end of class Day 3

---
+++ app.js
@@ -0,0 +1,34 @@
+const app ={
+    init: function(selectors){
+        this.flicks = []
+        this.max =0
+        this.list =document.querySelector(selectors.listSelector)
+
+        document
+            .querySelector(selectors.formSelector)
+            .addEventListener('submit', this.handleSubmit.bind(this))
+    },
+
+renderListItem: function(flick){
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    return item
+},
+    handleSubmit: function(ev){
+        ev.preventDefault()
+        const f = ev.target
+       const flick ={
+           id: this.max +1,
+           name: f.flickName.value,
+       }
+
+       this.list.appendChild(this.renderListItem(flick))
+
+        this.max ++
+    },
+}
+
+app.init({
+    formSelector:'form#flick-form',
+    listSelector: '#flick-list',
+})
