0fb20136125c662dce141afa3efa332203501b41
Joseph Carlson <guitarjoe96@yahoo.com>
2017-07-02 22:31:21 -0400

    Added images

--- app.js
+++ app.js
@@ -58,51 +58,6 @@ removeFlick(flick, ev)
 
 
 
-renderFavButton: function(){
-    const fav = document.createElement('BUTTON')
-    fav.setAttribute('class','button alert')
-    fav.style.position='absolute'
-    fav.style.right='175px'
-    fav.style.padding = '10px'
-    fav.style.backgroundColor='gold'
-    
-    fav.textContent ='Favorite'
-   
-    return fav
-},
-renderDeleteButton: function(){
-    const del = document.createElement('BUTTON')
-    del.setAttribute('class','button alert')
-    del.style.position='absolute'
-    del.style.right='100px'
-    del.style.padding = '10px'
-    del.style.backgroundColor='red'
-    
-    del.textContent ='Delete'
-    return del
-    
-},
-
-renderUpButton: function(){
-    const up = document.createElement('BUTTON')
-    up.setAttribute('class','button')
-    up.style.position='absolute'
-    up.style.right='261px'
-    up.style.padding = '10px'
-    
-    up.textContent ='↑'
-    return up
-},
-renderDownButton: function(){
-    const down = document.createElement('BUTTON')
-    down.setAttribute('class','button alert')
-    down.style.position='absolute'
-    down.style.right='301px'
-    down.style.padding = '10px'
-    
-    down.textContent ='↓'
-    return down
-},
 
     handleSubmit: function(ev){
         ev.preventDefault()
